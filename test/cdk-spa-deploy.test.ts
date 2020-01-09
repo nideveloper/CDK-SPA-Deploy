08305a564ed6eeedd50c99b51759474a291d05bc
EC2 Default User <ec2-user@ip-172-31-82-240.ec2.internal>
2020-01-09 21:49:10 +0000

    better tests

--- test/cdk-spa-deploy.test.ts
+++ test/cdk-spa-deploy.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, haveResourceLike } from '@aws-cdk/assert';
 import { Stack } from '@aws-cdk/core';
 import { SPADeploy } from '../lib/';
 
@@ -12,11 +12,56 @@ test('Cloudfront Distribution Included', () => {
       websiteFolder: 'website'
     })
     // THEN
-    expectCDK(stack).to(haveResource('AWS::CloudFront::Distribution'));
+    expectCDK(stack).to(haveResource('AWS::S3::Bucket', {
+      WebsiteConfiguration: {
+        IndexDocument: 'index.html'
+      }
+    }));
+    
+    expectCDK(stack).to(haveResource('Custom::CDKBucketDeployment'));
+    
+    expectCDK(stack).to(haveResourceLike('AWS::CloudFront::Distribution', {
+      "DistributionConfig": {
+          "CustomErrorResponses": [
+            {
+              "ErrorCode": 403,
+              "ResponseCode": 200,
+              "ResponsePagePath": "/index.html"
+            },
+            {
+              "ErrorCode": 404,
+              "ResponseCode": 200,
+              "ResponsePagePath": "/index.html"
+            }
+          ],
+          "DefaultCacheBehavior": {
+                "ViewerProtocolPolicy": "redirect-to-https"
+          },
+          "DefaultRootObject": "index.html",
+          "HttpVersion": "http2",
+          "IPV6Enabled": true,
+          "PriceClass": "PriceClass_100",
+          "ViewerCertificate": {
+            "CloudFrontDefaultCertificate": true
+          }
+      }
+    }));
+    
+    expectCDK(stack).to(haveResourceLike('AWS::S3::BucketPolicy',  {
+            PolicyDocument: {
+                Statement: [
+                    {
+                        "Action": "s3:GetObject",
+                        "Effect": "Allow",
+                        "Principal": "*"
+                    }]
+            }
+    }));
 });
 
 test('Basic Site Setup', () => {
     let stack = new Stack();
+    
     // WHEN
     let deploy = new SPADeploy(stack, 'spaDeploy');
     
@@ -24,7 +69,24 @@ test('Basic Site Setup', () => {
       indexDoc: 'index.html',
       websiteFolder: 'website'
     })
+    
     // THEN
-    expectCDK(stack).to(haveResource('AWS::S3::Bucket'));
+    expectCDK(stack).to(haveResource('AWS::S3::Bucket', {
+      WebsiteConfiguration: {
+        IndexDocument: 'index.html'
+      }
+    }));
+    
     expectCDK(stack).to(haveResource('Custom::CDKBucketDeployment'));
+    
+    expectCDK(stack).to(haveResourceLike('AWS::S3::BucketPolicy',  {
+            PolicyDocument: {
+                Statement: [
+                    {
+                        "Action": "s3:GetObject",
+                        "Effect": "Allow",
+                        "Principal": "*"
+                    }]
+            }
+    }));
 });
